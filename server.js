3771f2aa98e9b0f1a3381be49fea6c7ce387bd49
Elijah Insua <tmpvar@gmail.com>
2014-08-21 16:58:04 -0700

    spawn up oce

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+var argv = require('optimist').argv;
+var spawn = require('child_process').spawn;
+var path = require('path');
 var skateboard = require('skateboard');
 
-skateboard(function(stream) {
-  console.log('connection :)')
+if (!argv.oce) {
+  return console.log('usage: livecad --oce=/path/to/net-oce');
+}
+
+skateboard({
+  port : 9971,
+  dir: path.join(__dirname, 'static')
+}, function(stream) {
+
+  var oce = spawn(path.resolve(process.cwd(), argv.oce), [], { stdio: 'pipe' });
+  stream.pipe(oce.stdin);
+  oce.stdout.pipe(stream);
 })
