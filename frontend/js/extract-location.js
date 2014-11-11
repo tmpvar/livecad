44f5faf6dfe3e02cd460ee475e527fc6a71b4106
Elijah Insua <tmpvar@gmail.com>
2014-11-10 22:48:47 -0800

    fix extract location to work as expected

--- frontend/js/extract-location.js
+++ frontend/js/extract-location.js
@@ -2,7 +2,7 @@ module.exports = extractLocation;
 
 function extractLocation(lineOffset, columnOffset) {
   lineOffset = lineOffset || -6;
-  columnOffset = columnOffset || -3;
+  columnOffset = columnOffset || -1;
 
   var currentLine = (new Error()).stack.split('\n').filter(function(line) {
     return line.indexOf('eval at <anonymous>') > -1;
@@ -10,8 +10,8 @@ function extractLocation(lineOffset, columnOffset) {
 
   if (currentLine) {
     var parts = currentLine.split(':');
-    var col = Math.max(0, parseInt(parts.pop(), 10) - columnOffset);
-    var line = parseInt(parts.pop(), 10) - lineOffset;
-    return [col, line];
+    var col = Math.max(0, parseInt(parts.pop(), 10) + columnOffset);
+    var line = parseInt(parts.pop(), 10) + lineOffset;
+    return [line, col];
   }
 }
