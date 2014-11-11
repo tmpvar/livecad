2ce0daf837fe9540f02fce9b2f64ebd7c62266b7
Elijah Insua <tmpvar@gmail.com>
2014-11-11 02:48:13 -0800

    add EventEmitter to Shape 'class'

--- frontend/js/shape.js
+++ frontend/js/shape.js
@@ -1,3 +1,5 @@
+var EventEmitter = require('events').EventEmitter;
+
 module.exports = Shape;
 
 function Shape() {
@@ -5,9 +7,12 @@ function Shape() {
 }
 
 Shape.shapeId = 1;
+Shape.emitter = new EventEmitter();
 
 Shape.prototype.id = 0;
 Shape.prototype.line = 0;
 Shape.prototype.column = 0;
 Shape.prototype.isShape = true;
+Shape.prototype.name = 'unknown';
+Shape.prototype.parent = null;
 // TODO: either extend from renderable or compose over one
